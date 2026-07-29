247df3271e9bf79a8f7c58754d0494001ae34255
sinvijay <sivijay@proton.me>
2026-07-29 15:24:54 +0530

    a

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // If running Django locally, it's usually localhost:8000
-    // Once deployed, change this to your Railway URL
-    baseURL: 'http://localhost:5173/ , https://synergyapp.up.railway.app/'
+  // This looks for VITE_API_URL. If it's missing (local), it defaults to localhost.
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
 });
 
 export default api;
