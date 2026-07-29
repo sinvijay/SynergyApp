d1f6f193ca2f9374f19a223ce4babf884256f5e9
sinvijay <sivijay@proton.me>
2026-07-29 15:21:58 +0530

    a

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // If running Django locally, it's usually localhost:8000
     // Once deployed, change this to your Railway URL
-    baseURL: 'http://localhost:5173/'
+    baseURL: 'http://localhost:5173/ , https://synergyapp.up.railway.app/'
 });
 
 export default api;
